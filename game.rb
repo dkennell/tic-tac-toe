2f78fb6dfe281e5f9d42571cef9cce9a17793d45
David Kennell <davidkennell@gmail.com>
2019-09-26 22:44:41 -0500

    Get input for use in token choice

--- game.rb
+++ game.rb
@@ -1,3 +1,6 @@
 require 'paint'
 
-puts Paint["You're doing really well so far David, good work!", :magenta]
+puts Paint["Welcome to Tic Tac Toe! Would you like to play as X or O?", :magenta]
+puts Paint["(Please enter X or O and then hit enter)", :yellow]
+a = gets.strip()
+puts "you said " + a
