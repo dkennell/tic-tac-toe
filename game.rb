4c4ef1aba9f8edeaed8cc718e00131a34a8c3596
David Kennell <davidkennell@gmail.com>
2019-09-27 22:51:21 -0500

    Add play_game and print_board methods

--- game.rb
+++ game.rb
@@ -1,7 +1,8 @@
 require 'paint'
-require './game_library'
-include GameLibrary
+require './lib/game_engine'
+include GameEngine
 
 welcome_player()
 ask_player_for_token_choice()
-get_player_token_choice()
+token = get_player_token_choice()
+play_game(token)
--- lib/game_engine.rb
+++ lib/game_engine.rb
@@ -1,4 +1,17 @@
-module GameLibrary
+module GameEngine
+  def play_game(token)
+    puts "Please choose your next move"
+    print_board()
+  end
+
+  def print_board
+    puts "1 | 2 | 3 "
+    puts "----------"
+    puts "4 | 5 | 6 "
+    puts "----------"
+    puts "7 | 8 | 9 "
+  end
+
   def welcome_player
     puts Paint["Welcome to Tic Tac Toe!", :magenta]
   end
@@ -9,7 +22,6 @@ module GameLibrary
 
   def get_player_token_choice
     puts Paint["(Please enter X or O and then hit enter)", :yellow]
-    a = gets.strip()
-    puts "you said " + a
+    gets.strip()
   end
 end
